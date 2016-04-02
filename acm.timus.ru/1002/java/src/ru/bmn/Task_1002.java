2273a1069f6873d5936a90cea91e538e4fa633be
bmn <mnb.anonimus@gmail.com>
2016-04-03 02:03:41 +0300

    java-1002

--- acm.timus.ru/1002/java/src/ru/bmn/Task_1002.java
+++ acm.timus.ru/1002/java/src/ru/bmn/Task_1002.java
@@ -1,6 +1,7 @@
 package ru.bmn;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.HashMap;
 
 
@@ -40,46 +41,71 @@ class TaskWrapper {
             this.in.nextToken();
             Integer helperSize  = (int)this.in.nval;
 
-            Trie t = new Trie();
+            Trie t = new Trie('\0');
             for (int i = 0; i < helperSize; i++) {
                 this.in.nextToken();
                 t.addWord(this.in.sval);
             }
 
-            this.out.println(this.findWords(targetNumber));
+            this.out.println(this.findWords(t, targetNumber));
         }
 
         this.out.flush();
         this.out.close();
     }
 
-    private String findWords(String targetNumber) {
+    private String findWords(Trie t, String targetNumber) {
+        char[] digits = targetNumber.toCharArray();
+        t.findWord(digits);
         return "No solution.";
     }
+
+
 }
 
 class Trie {
-    private Character value;
-    private Character realValue;
-    private boolean   isLeaf;
-    private HashMap<Character, Trie> childs;
-    private HashMap<Character, Trie> root = new HashMap<Character, Trie>();
+    protected Character value;
+    protected boolean isLeaf = false;
+    protected String word;
+    protected HashMap<Character, Trie> childs;
 
-    Trie() {
-        this.root.put('\0', null);
+    Trie(Character c) {
+        this.value = c;
+        this.childs = new HashMap<>();
     }
 
     public void addWord(String word) {
         char[] chars = word.toCharArray();
 
+        Trie currNode = this;
         for (Character c: chars) {
-            this.addChar(c);
+            currNode = this.addChar(c, currNode);
         }
-
+        currNode.isLeaf = true;
+        currNode.word   = word;
     }
 
-    private void addChar(Character c) {
+    private Trie addChar(Character c, Trie currNode) {
+        if (currNode.childs.containsKey(c)) {
+            return currNode.childs.get(c);
+        }
+        else {
+            currNode.childs.put(c, new Trie(c));
+            return currNode.childs.get(c);
+        }
+    }
 
+    public String findWord(char[] digits) {
+        String result = null;
 
+        if (this.childs.containsKey(digits[0])) {
+            if ((digits.length == 1) && this.childs.get(digits[0]).isLeaf) {
+                return this.childs.get(digits[0]).word;
+            }
+            else if (digits.length > 1) {
+                return this.childs.get(digits[0]).findWord(Arrays.copyOfRange(digits, 1, digits.length));
+            }
+        }
+        return result;
     }
 }
