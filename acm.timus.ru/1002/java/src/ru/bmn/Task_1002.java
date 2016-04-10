bf7fa81f2d99a8eb8fb80e034112a84976433f3c
bmn <mnb.anonimus@gmail.com>
2016-04-10 18:19:19 +0300

    java-1002

--- acm.timus.ru/1002/java/src/ru/bmn/Task_1002.java
+++ acm.timus.ru/1002/java/src/ru/bmn/Task_1002.java
@@ -17,6 +17,8 @@ class TaskWrapper {
     StreamTokenizer in;
     PrintWriter     out;
 
+
+
     TaskWrapper() throws IOException {
         this.isOnlineJudge = System.getProperty("ONLINE_JUDGE") != null;
 
@@ -36,7 +38,8 @@ class TaskWrapper {
         while (this.in.nextToken() != StreamTokenizer.TT_EOF) {
             if (this.in.nval == -1) break;
 
-            String targetNumber = this.in.sval;
+            Double targetNumber = this.in.nval;
+            String targetNumberStr = Long.toString(targetNumber.longValue());
 
             this.in.nextToken();
             Integer helperSize  = (int)this.in.nval;
@@ -47,17 +50,27 @@ class TaskWrapper {
                 t.addWord(this.in.sval);
             }
 
-            this.out.println(this.findWords(t, targetNumber));
+            this.out.println(this.findWords(t, targetNumberStr.toCharArray()));
         }
 
         this.out.flush();
         this.out.close();
     }
 
-    private String findWords(Trie t, String targetNumber) {
-        char[] digits = targetNumber.toCharArray();
-        t.findWord(digits);
-        return "No solution.";
+    private String findWords(Trie t, char[] digitPath) {
+		char[] dp = digitPath;
+
+		while (dp.length > 0) {
+			String word = t.findWord(dp);
+			if (word == null) {
+				if (dp.length > 1) {
+					// Ищем слово поменьше
+					dp = Arrays.copyOfRange(digitPath, 0, digitPath.length - 1);
+				}
+			}
+		}
+
+        return phrase == null ? "No solution." : word;
     }
 
 
@@ -65,33 +78,64 @@ class TaskWrapper {
 
 class Trie {
     protected Character value;
-    protected boolean isLeaf = false;
     protected String word;
     protected HashMap<Character, Trie> childs;
+	private static final HashMap<Character, Character> DIGIT_MAP = new HashMap<Character, Character>() {{
+		put('a', '2');
+		put('b', '2');
+		put('c', '2');
+		put('d', '3');
+		put('e', '3');
+		put('f', '3');
+		put('i', '1');
+		put('j', '1');
+		put('g', '4');
+		put('h', '4');
+		put('k', '5');
+		put('l', '5');
+		put('m', '6');
+		put('n', '6');
+		put('p', '7');
+		put('r', '7');
+		put('s', '7');
+		put('t', '8');
+		put('u', '8');
+		put('v', '8');
+		put('w', '9');
+		put('x', '9');
+		put('y', '9');
+		put('o', '0');
+		put('q', '0');
+		put('z', '0');
+	}};
 
-    Trie(Character c) {
+	Trie(Character c) {
         this.value = c;
         this.childs = new HashMap<>();
     }
 
-    public void addWord(String word) {
+	private Character charToDigit(Character c) {
+		return DIGIT_MAP.get(c);
+	}
+
+	public void addWord(String word) {
         char[] chars = word.toCharArray();
 
         Trie currNode = this;
         for (Character c: chars) {
             currNode = this.addChar(c, currNode);
         }
-        currNode.isLeaf = true;
-        currNode.word   = word;
+        currNode.word = word;
     }
 
     private Trie addChar(Character c, Trie currNode) {
-        if (currNode.childs.containsKey(c)) {
-            return currNode.childs.get(c);
+		Character digit = DIGIT_MAP.get(c);
+        if (currNode.childs.containsKey(digit)) {
+            return currNode.childs.get(digit);
         }
         else {
-            currNode.childs.put(c, new Trie(c));
-            return currNode.childs.get(c);
+            currNode.childs.put(digit, new Trie(digit));
+            return currNode.childs.get(digit);
         }
     }
 
@@ -99,7 +143,7 @@ class Trie {
         String result = null;
 
         if (this.childs.containsKey(digits[0])) {
-            if ((digits.length == 1) && this.childs.get(digits[0]).isLeaf) {
+            if ((digits.length == 1) && this.childs.get(digits[0]).word != null) {
                 return this.childs.get(digits[0]).word;
             }
             else if (digits.length > 1) {
---
+++ acm.timus.ru/1002/java/src/ru/bmn/TrieTest.java
@@ -0,0 +1,17 @@
+package ru.bmn;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by bmn85 on 09.04.2016.
+ */
+public class TrieTest extends TestCase {
+	public void testFindWord() throws Exception {
+		Trie t = new Trie('\0');
+		t.addWord("cat");
+
+		assertEquals(t.findWord(new char[] {'1', '2', '4'}), null);
+
+		assertEquals(t.findWord(new char[] {'2', '2', '8'}), "cat");
+	}
+}
